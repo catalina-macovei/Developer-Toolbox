5b981fe3bb88a8a231efd90eb99be1988f41a7ff
Catalina Macovei <cmacovei01@gmail.com>
2024-04-10 11:51:21 +0300

    update cotroller view solutions, tags

---
+++ Controllers/SolutionsController.cs
@@ -0,0 +1,42 @@
+﻿using Developer_Toolbox.Data;
+using Developer_Toolbox.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Developer_Toolbox.Controllers
+{
+    public class SolutionsController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        public SolutionsController(ApplicationDbContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            var solutions = from solution in db.Solutions
+                            orderby solution.Score
+                            select solution;
+
+            ViewBag.Solutions = solutions;
+
+            return View();
+        }
+
+        public IActionResult Show(int id) 
+        { 
+            Solution solution = db.Solutions.Find(id);
+
+            return View(solution);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            Solution solution = db.Solutions.Find(id);
+            db.Solutions.Remove(solution);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
